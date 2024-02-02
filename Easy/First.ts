a0c7e8c76352f384169438278753be8a732c2128
Thiago Augusto <150065566+tapsouza@users.noreply.github.com>
2024-02-02 16:52:00 +0000

    Create First.ts

---
+++ Easy/First.ts
@@ -0,0 +1,24 @@
+/*
+  Implement a generic `First<T>` that takes an Array `T` and returns its first element's type.
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type First<T extends any[]> = T extends [infer NumberOne, ...T[number]] ? NumberOne : never
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<First<[3, 2, 1]>, 3>>,
+  Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
+  Expect<Equal<First<[]>, never>>,
+  Expect<Equal<First<[undefined]>, undefined>>,
+]
+
+type errors = [
+  // @ts-expect-error
+  First<'notArray'>,
+  // @ts-expect-error
+  First<{ 0: 'arrayLike' }>,
+]
