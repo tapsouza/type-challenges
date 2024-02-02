88b33391be5e2d82a76c9cc268c9bc68d98e51af
Thiago Augusto <150065566+tapsouza@users.noreply.github.com>
2024-02-02 17:33:29 +0000

    Create Push.ts

---
+++ Easy/Push.ts
@@ -0,0 +1,16 @@
+/*
+  Implement the generic version of ```Array.push```
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Push<T extends any[], U> = [...T, U]
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Push<[], 1>, [1]>>,
+  Expect<Equal<Push<[1, 2], '3'>, [1, 2, '3']>>,
+  Expect<Equal<Push<['1', 2, '3'], boolean>, ['1', 2, '3', boolean]>>,
+]
