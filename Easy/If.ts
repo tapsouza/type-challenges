119a13590c46be390c0c6e534a7911c9dbae9629
Thiago Augusto <150065566+tapsouza@users.noreply.github.com>
2024-02-02 17:20:50 +0000

    Create If.ts

---
+++ Easy/If.ts
@@ -0,0 +1,18 @@
+/*
+  Implement the util type `If<C, T, F>` which accepts condition `C`, a truthy value `T`, and a falsy value `F`. `C` is expected to be either `true` or `false` while `T` and `F` can be any type.
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type If<C extends boolean, T, F> = C extends true ? T : F
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<If<true, 'a', 'b'>, 'a'>>,
+  Expect<Equal<If<false, 'a', 2>, 2>>,
+]
+
+// @ts-expect-error
+type error = If<null, 'a', 'b'>
