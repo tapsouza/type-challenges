cee0c38cebdbb60b0667be924b7aff828d64ce11
Thiago Augusto <150065566+tapsouza@users.noreply.github.com>
2024-02-06 22:09:35 +0000

    Create Parameters.ts

---
+++ Easy/Parameters.ts
@@ -0,0 +1,20 @@
+/*
+  Implement the built-in Parameters<T> generic without using it.
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type MyParameters<T> = T extends (...args: infer Params) => any ? Params : never
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+function foo(arg1: string, arg2: number): void {}
+function bar(arg1: boolean, arg2: { a: 'A' }): void {}
+function baz(): void {}
+
+type cases = [
+  Expect<Equal<MyParameters<typeof foo>, [string, number]>>,
+  Expect<Equal<MyParameters<typeof bar>, [boolean, { a: 'A' }]>>,
+  Expect<Equal<MyParameters<typeof baz>, []>>,
+]
